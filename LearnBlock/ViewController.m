2a0f4fecdb36b85f941e8508c13067b71e5a8170
chiyz <3004346919@qq.com>
2019-05-27 15:14:28 +0800

    feat：初始化项目

---
+++ LearnBlock/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  LearnBlock
+//
+//  Created by chiyz on 2019/5/27.
+//  Copyright © 2019年 chiyz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ LearnBlock/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearnBlock
+//
+//  Created by chiyz on 2019/5/27.
+//  Copyright © 2019年 chiyz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
